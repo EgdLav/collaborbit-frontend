a0aed75b7782ef60c8596dcd8109a4ba2f9a03d2
egor <egd.lavrinenko@gmail.com>
2026-07-18 13:43:21 +0300

    echo modified

--- src/plugins/echo.ts
+++ src/plugins/echo.ts
@@ -1,19 +1,23 @@
 import Echo from 'laravel-echo'
 import Pusher from 'pusher-js'
 
-(window as any).Pusher = Pusher
+;(window as any).Pusher = Pusher
 
 export const echo = new Echo({
   broadcaster: 'reverb',
+
   key: import.meta.env.VITE_REVERB_APP_KEY,
 
-  wsHost: import.meta.env.VITE_REVERB_HOST ?? 'https://collaborbit.mooo.com',
-  wsPort: 80,
+  wsHost: window.location.hostname,
 
-  forceTLS: false,
+  wsPort: 443,
+  wssPort: 443,
+
+  forceTLS: true,
+
   enabledTransports: ['ws', 'wss'],
 
-  authEndpoint: 'https://collaborbit.mooo.com/broadcasting/auth',
+  authEndpoint: '/broadcasting/auth',
 
   auth: {
     headers: {
